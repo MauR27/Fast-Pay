5832a75f03cfe3945cd7788aa5af79f819e2bc0e
MauR27 <antonio27mauricio27@gmail.com>
2025-04-07 17:00:57 +0200

    added middleware

---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+
+const handler = async (req: NextRequest) => {
+  try {
+    const session = await getToken({
+      req,
+      secret: process.env.NEXTAUTH_SECRET,
+    });
+
+    if (!session) {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+    if (
+      req.nextUrl.pathname === "/signup" &&
+      session.email !== process.env.ADMIN_ROLE
+    ) {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+
+    return NextResponse.next();
+  } catch (error) {
+    if (error instanceof Error) throw new Error(error.message);
+  }
+};
+export default handler;
+
+export const config = {
+  matcher: ["/signup"],
+};
